808c5803d4ebfedc009962295e80c241cd774f89
Felix Schuller <e1025256@student.tuwien.ac.at>
2015-04-05 12:37:15 +0200

    Init Project w. basic structure

---
+++ setup_space.bat
@@ -0,0 +1,1 @@
+java -Dlogback.configurationFile=logback.xm -cp libs/mozartspaces-dist-2.3-SNAPSHOT-r14098-all-with-dependencies.jar org.mozartspaces.core.Server 12345
---
+++ src/TUI/SimpleSpaceViewer.java
@@ -0,0 +1,11 @@
+package TUI;
+
+/**
+ * Created by Felix on 05.04.2015.
+ */
+public class SimpleSpaceViewer {
+
+    public static void main (String[] args) {
+        //Fill in
+    }
+}
